6226257aabe36087c64bf8c4e45d918be53a8a78
radityadias <radityadias24@gmail.com>
2025-09-24 17:09:27 +0700

    update navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,23 +31,11 @@ export default function RootLayout({
           <SidebarProvider>
               <AppSidebar />
               <SidebarInset>
-                  <header className="flex h-16 shrink-0 items-center gap-2 border-b">
-                      <div className="flex items-center gap-2 px-3">
+                  <header className="flex h-16 shrink-0 items-center gap-2 bg-blue-accent">
+                      <div className="flex items-center gap-2 px-3 text-white-primary">
                           <SidebarTrigger />
                           <Separator orientation="vertical" className="mr-2 h-4" />
-                          <Breadcrumb>
-                              <BreadcrumbList>
-                                  <BreadcrumbItem className="hidden md:block">
-                                      <BreadcrumbLink href="#">
-                                          Building Your Application
-                                      </BreadcrumbLink>
-                                  </BreadcrumbItem>
-                                  <BreadcrumbSeparator className="hidden md:block" />
-                                  <BreadcrumbItem>
-                                      <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                                  </BreadcrumbItem>
-                              </BreadcrumbList>
-                          </Breadcrumb>
+                          <p className="font-medium text-lg">Build your time!</p>
                       </div>
                   </header>
 
